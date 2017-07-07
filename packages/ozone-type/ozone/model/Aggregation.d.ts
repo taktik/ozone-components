f83484be54acadb3fe8544e8902386e16f429f9f
Hubert Jacquin <hj@taktik.com>
2017-07-07 16:05:24 +0200

    change $Type to $type

--- packages/ozone-type/ozone/model/Aggregation.d.ts
+++ packages/ozone-type/ozone/model/Aggregation.d.ts
@@ -1,3 +1,3 @@
 export interface Aggregation {
-    $Type: string;
+    $type: string;
 }
--- packages/ozone-type/ozone/model/Aggregation.ts
+++ packages/ozone-type/ozone/model/Aggregation.ts
@@ -13,6 +13,6 @@
 import * as models from './models';
 
 export interface Aggregation {
-    $Type: string;
+    $type: string;
 
 }
