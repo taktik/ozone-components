756d5e1e563e17b847410c8e40e57fdc64d07899
Hubert Jacquin <hj@taktik.com>
2017-08-16 16:51:49 +0200

    remove splited directories

--- packages/ozone-upload/ozone-upload.js
+++ packages/ozone-upload/ozone-upload.js
@@ -7,7 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-define("ozone-upload", ["require", "exports", "decorators", "ozone-api-upload"], function (require, exports, taktik_polymer_typeScript_1, ozone_api_upload_1) {
+define("ozone-upload", ["require", "exports", "taktik-polymer-typeScript", "ozone-api-upload"], function (require, exports, taktik_polymer_typeScript_1, ozone_api_upload_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /**
