b8aa332a9dbbb584a567254f08b800893d9df889
hj <hj@taktik.com>
2018-04-26 23:15:23 +0200

    improve ozone-item-action event management

--- packages/ozone-material/ozone-item-preview/src/ozone-item-action/ozone-item-action.ts
+++ packages/ozone-material/ozone-item-preview/src/ozone-item-action/ozone-item-action.ts
@@ -16,21 +16,24 @@ import {OzoneMediaUrl, OzonePreviewSize, SizeEnum} from 'ozone-media-url'
 import {OzoneApiType, getOzoneApiType} from 'ozone-api-type'
 
 /**
- * `ozone-item-preview` is hight level polymer module to display preview information an ozone item.
+ * `ozone-item-action` display action preview.
  *
  * Example in html:
  * ```html
- * <ozone-item-preview itemData=[[item]]></ozone-item-preview>
+ * <ozone-item-action itemData=[[item]]></ozone-item-action>
  * ```
  *
  * ### Events
  *
- * * *edit-item* fire on click on close button.
+ * * *edit-item*
+ * * *delete-item*
+ * * *info-item*
  *
  *  ### Mixin
  *  Custom property | Description | Default
  *  ----------------|-------------|----------
- *  --ozone-item-preview | css mixin for preview container | `{}`
+ *  --actions-panel-colors | css mixin for action panel | `{...}`
+ *  --actions-panel-icons-color | css mixin for action icon | `{color:white;min-height:30px; min-width:30px;}`
  */
 @customElement('ozone-item-action')
 export class OzoneItemAction extends Polymer.Element{
@@ -45,31 +48,23 @@ export class OzoneItemAction extends Polymer.Element{
     @property({type: Boolean})
     selected: boolean = false;
 
-    @property({type: String})
-    action: string = 'nop';
-
-    private _editItem(e: Event){
-        this.dispatchEvent(new CustomEvent('edit-item',
-            {bubbles: true, detail:this.itemData, composed: true} as CustomEventInit));
-
-        e.preventDefault();
-        e.stopPropagation();
-    }
-    private _infoItem(e: Event){
-        this.set('action', 'info-item')
-        this.dispatchEvent(new CustomEvent('info-item',
-            {bubbles: true, detail:this.itemData, composed: true} as CustomEventInit));
-
-        e.preventDefault();
-        e.stopPropagation();
-    }
-
-    private _delete(e: Event){
-        this.dispatchEvent(new CustomEvent('delete-item',
-            {bubbles: true, detail:this.itemData, composed: true} as CustomEventInit));
-
-        e.preventDefault();
-        e.stopPropagation();
+    ready() {
+        super.ready();
+        if(this.shadowRoot) {
+            const actions = this.shadowRoot.querySelectorAll<HTMLElement>('.action')
+            Array.from(actions).forEach((action: HTMLElement) => {
+                action.addEventListener('tap', (e: Event) => {
+                    const event = e as MouseEvent
+                    if(event.target && (event.target as HTMLElement).hasAttribute('event')){
+                        const eventName = (event.target as HTMLElement).getAttribute('event') as string
+                        this.dispatchEvent(new CustomEvent(eventName,
+                            {bubbles: true, detail: this.itemData, composed: true} as CustomEventInit));
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }
+                })
+            })
+        }
     }
 
     @observe('selected')
