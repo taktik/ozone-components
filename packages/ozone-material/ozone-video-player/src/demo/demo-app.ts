bed0abe4cb5c8246f4b88141af27e2e56d59925e
hjacquin <hj@taktik.com>
2018-11-29 14:56:37 +0100

    Fix unit test

--- packages/ozone-material/ozone-video-player/src/demo/demo-app.ts
+++ packages/ozone-material/ozone-video-player/src/demo/demo-app.ts
@@ -59,11 +59,10 @@ class demoApp extends Polymer.Element {
     _loadOzoneVideo(){
         if(!this.$)
             throw new Error();
-        const video: Video = Object.assign(new Video(),
-        {
-            id: '00000000-0000-0000-0000-000000000003',
-            subTitle:{ "English": "00000000-0000-0000-0000-000000000002","Polish": "00000000-0000-0000-0000-000000000001" },
-        });
+        const video: Video = new Video({
+                id: '00000000-0000-0000-0000-000000000003',
+                subTitle:{ "English": "00000000-0000-0000-0000-000000000002","Polish": "00000000-0000-0000-0000-000000000001" },
+            });
         this.$.mediaPlayer.loadOzoneVideo(video);
     }
 
