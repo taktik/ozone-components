8f91df4e080603027ef5bded658d285802e62163
hjacquin <hj@taktik.com>
2019-04-30 14:08:53 +0200

    Fix DeviceMessageTakescreenshot type definition

--- packages/ozone-helper/ozone-type/ozone/model/DeviceMessageTakescreenshot.ts
+++ packages/ozone-helper/ozone-type/ozone/model/DeviceMessageTakescreenshot.ts
@@ -1,17 +1,15 @@
-import { DeviceMessage } from './DeviceMessage'
+import { DeviceMessageFromdevice } from './DeviceMessageFromdevice'
 
 import { Item, UUID, Instant, OzoneType } from './Item'
 
 @OzoneType('device.message.takescreenshot')
-export class DeviceMessageTakescreenshot extends DeviceMessage {
-	delay?: number[]
-	inputDeviceId: UUID
+export class DeviceMessageTakescreenshot extends DeviceMessageFromdevice {
+	delay?: number
 	screenshotId?: string
 
 	constructor(src: DeviceMessageTakescreenshot) {
 		super(src)
 		this.delay = src.delay
-		this.inputDeviceId = src.inputDeviceId
 		this.screenshotId = src.screenshotId
 	}
 }
