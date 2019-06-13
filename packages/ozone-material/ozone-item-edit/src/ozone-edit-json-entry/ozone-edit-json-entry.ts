931d9fdb30c23e502f3dc1fb9089d5cd29beccef
hjacquin <hj@taktik.com>
2019-06-13 15:50:04 +0200

    Fix demo for ozone-edit-json-entry

--- packages/ozone-material/ozone-item-edit/src/ozone-edit-json-entry/ozone-edit-json-entry.ts
+++ packages/ozone-material/ozone-item-edit/src/ozone-edit-json-entry/ozone-edit-json-entry.ts
@@ -2,10 +2,10 @@
  * Created by hubert on 23/06/17.
  */
 
-import "paper-input/paper-textarea.html"
-import "./ozone-edit-json-entry.html"
-import {customElement, property} from 'taktik-polymer-typescript'
-import {OzoneEditEntry} from '../ozone-edit-entry/ozone-edit-entry'
+import 'paper-input/paper-textarea.html'
+import './ozone-edit-json-entry.html'
+import { customElement, property } from 'taktik-polymer-typescript'
+import { OzoneEditEntry } from '../ozone-edit-entry/ozone-edit-entry'
 
 /**
  * <ozone-edit-json-entry> is an element to edit ozone items fields as a json.
@@ -13,44 +13,45 @@ import {OzoneEditEntry} from '../ozone-edit-entry/ozone-edit-entry'
  */
 @customElement('ozone-edit-json-entry')
 export class OzoneEditJsonEntry extends OzoneEditEntry {
-    @property({type: String})
-    textValue?:string;
+	@property({ type: String })
+	textValue?: string
 
-    static get observers() {
-        return [
-            'valueChange(value)',
-            'textChange(textValue)'
-        ]
-    }
-    registerChangeListener (){
-        // NOP prevent default behavior that listen on change event
-    }
+	static get observers() {
+		return [
+			'valueChange(value)',
+			'textChange(textValue)'
+		]
+	}
+	registerChangeListener () {
+		// NOP prevent default behavior that listen on change event
+	}
 
-    jsonTotext(rawValue:object):string{
-        return JSON.stringify(rawValue);
-    }
+	jsonTotext(rawValue: object): string {
+		return JSON.stringify(rawValue)
+	}
 
-    isValueAndTextEqual():boolean{
-        let value = this.value || {};
-        return this.textValue === this.jsonTotext(value);
-    }
+	isValueAndTextEqual(): boolean {
+		let value = this.value || {}
+		return this.textValue === this.jsonTotext(value)
+	}
 
-    valueChange () {
-        if(! this.isValueAndTextEqual()){
-            this.set('textValue', this.jsonTotext(this.value));
-        }
-    }
+	valueChange () {
+		if (! this.isValueAndTextEqual()) {
+			this.set('textValue', this.jsonTotext(this.value))
+		}
+	}
 
-    textChange () {
-        try{
-            const value = JSON.parse(this.textValue as string)
-            if(! this.isValueAndTextEqual()){
-                this.set('value', value);
-                this.set('isModify', true);
-                this.set('invalid', false)
-            }
-        } catch (e) {
-            this.set('invalid', true)
-        }
-    }
+	textChange () {
+		try {
+			const value = JSON.parse(this.textValue as string)
+			if (! this.isValueAndTextEqual()) {
+				this.set('value', value)
+				this.set('isModify', true)
+				this.set('invalid', false)
+			}
+		} catch (e) {
+			this.$.input.errorMessage = e.message
+			this.set('invalid', true)
+		}
+	}
 }
