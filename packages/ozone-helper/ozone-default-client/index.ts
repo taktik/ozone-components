1854a088b79c44f714c81a6a3d954d0bdedda49d
hubert <hj@taktik.com>
2019-08-20 14:29:50 +0200

    Adapt demo with new login/logout components

--- packages/ozone-helper/ozone-default-client/index.ts
+++ packages/ozone-helper/ozone-default-client/index.ts
@@ -8,7 +8,7 @@ const sessionCredentials: SessionCredentials = new SessionCredentials()
 export function getDefaultClient(): OzoneClient.OzoneClient {
 	if (defaultClient === undefined) {
 		const config: OzoneClient.ClientConfiguration = {
-			ozoneURL: 'ozone', //https://test.flowr.dev/
+			ozoneURL: 'ozone',
 			ozoneCredentials: sessionCredentials
 		}
 		defaultClient = OzoneClient.newOzoneClient(config)
