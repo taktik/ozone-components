84f9e82abd4126a559e0eb6c7fdf69d9c90c317b
hubert <hj@taktik.com>
2020-03-13 10:31:34 +0100

    fit test

--- packages/ozone-helper/ozone-media-url/test/ozone-media-url_test.js
+++ packages/ozone-helper/ozone-media-url/test/ozone-media-url_test.js
@@ -51,10 +51,8 @@ describe('tool OzoneMediaUrl', function () {
 
             const element = new OzoneMediaUrl('00000000-046c-7fc4-0000-000000006028', config);
             element._getVideoFileType = sinon.stub().withArgs().returns([{id:'fileType', identifier:'org.taktik.filetype.flowr.video'}]);
-            element.getVideoUrl().then((url)=>{
-                expect(url).to.equal('/ozone/view/24616/org.taktik.filetype.flowr.video/index.m3u8');
-                done();
-            });
+            element.getVideoUrl().to.equal('/ozone/view/24616/org.taktik.filetype.flowr.video/index.m3u8');
+            done();
         });
     });
     describe('getVideoUrlMp4', function () {
