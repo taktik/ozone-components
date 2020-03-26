3f7aa0c402d7068a5735abe46926252558c324bf
hubert <hj@taktik.com>
2020-03-26 11:28:11 +0100

    Improve media ID conversion to numeric Id.

--- scripts/publishDoc.sh
+++ scripts/publishDoc.sh
@@ -3,6 +3,14 @@ bucket_name=taktik-flowr-doc/ozone-components
 version=`grep version lerna.json | sed -e 's/"version"://' | sed -e 's/"//g' | sed -e 's/[[:space:]]//g'`
 cloud_path=$bucket_name/$version
 gs_path=gs://$cloud_path
+latest_path=gs://$bucket_name/latest
+
+gsutil cp gs://$bucket_name/version_history.txt demo/version_history.txt
+echo $version >> demo/version_history.txt
+gsutil cp demo/version_history.txt gs://$bucket_name/version_history.txt
+
 echo publish documentation for version: $version to $gs_path
 gsutil -m cp -r -J demo $gs_path
+gsutil -m cp -r -J demo $latest_path
 echo Documentation avaliable at https://storage.googleapis.com/$cloud_path/index.html
+echo Documentation avaliable at https://storage.googleapis.com/$bucket_name/latest/index.html
