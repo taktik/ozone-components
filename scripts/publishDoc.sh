dc95f569a8481cde4a72f2b0939e46bbf85ecfd9
hubert <hj@taktik.com>
2020-04-10 10:57:05 +0200

    Use rsync to copy documentaion on gcloud

--- scripts/publishDoc.sh
+++ scripts/publishDoc.sh
@@ -10,7 +10,7 @@ echo $version >> demo/version_history.txt
 gsutil cp demo/version_history.txt gs://$bucket_name/version_history.txt
 
 echo publish documentation for version: $version to $gs_path
-gsutil -h "Cache-Control:no-cache,max-age=0" -m cp -r -J demo/* $gs_path
-gsutil -h "Cache-Control:no-cache,max-age=0" -m cp -r -J demo/* $latest_path
+gsutil -h "Cache-Control:no-cache,max-age=0" -m rsync -r -J demo $gs_path
+gsutil -h "Cache-Control:no-cache,max-age=0" -m rsync -r -J demo $latest_path
 echo Documentation avaliable at https://storage.googleapis.com/$cloud_path/index.html
 echo Documentation avaliable at https://storage.googleapis.com/$bucket_name/latest/index.html
