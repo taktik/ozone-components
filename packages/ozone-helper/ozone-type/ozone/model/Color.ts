6100fc425b2dd66d85b6a83d1cbd3ea741c2a378
Jason Van Malder <jvm@taktik.com>
2021-03-30 16:08:50 +0200

    Fix color attributes

--- packages/ozone-helper/ozone-type/ozone/model/Color.ts
+++ packages/ozone-helper/ozone-type/ozone/model/Color.ts
@@ -3,11 +3,11 @@ import { Item, OzoneType } from './Item'
 @OzoneType('flowr.color')
 export class FlowrColor extends Item {
 	name: string = '__customColorKey'
-	color: string
+	value: string
 
 	constructor(src: FlowrColor) {
 		super(src)
-		this.name = src.name
-		this.color = src.color
+		this.name = src.name ?? '__customColorKey'
+		this.value = src.value
 	}
 }
