2f750955a10642636b7c8bc1d69bf2fe5104238e
Julien Duquaine <jd@taktik.com>
2021-04-28 14:26:15 +0200

    add videoConfHost to flowrConnectConfig

--- packages/ozone-helper/ozone-type/ozone/model/FlowrConnectConfig.ts
+++ packages/ozone-helper/ozone-type/ozone/model/FlowrConnectConfig.ts
@@ -4,18 +4,20 @@ import { Item, OzoneType } from './Item'
 
 @OzoneType('flowr.connect.config')
 export class FlowrConnectConfig extends Item {
+	videoConfHost: string
 	storageUnit: string
-    inputChannels: string[]
+	inputChannels: string[]
 	quotas: FlowrConnectConfigQuotas
-    tvUserExpirationDelayAfterLastActivityDays: number
-    features: FlowrConnectFeatures
+	tvUserExpirationDelayAfterLastActivityDays: number
+	features: FlowrConnectFeatures
 
 	constructor(src: FlowrConnectConfig) {
 		super(src)
+		this.videoConfHost = src.videoConfHost
 		this.storageUnit = src.storageUnit
 		this.inputChannels = src.inputChannels
-        this.quotas = src.quotas
-        this.tvUserExpirationDelayAfterLastActivityDays = src.tvUserExpirationDelayAfterLastActivityDays
-        this.features = src.features
+		this.quotas = src.quotas
+		this.tvUserExpirationDelayAfterLastActivityDays = src.tvUserExpirationDelayAfterLastActivityDays
+		this.features = src.features
 	}
 }
