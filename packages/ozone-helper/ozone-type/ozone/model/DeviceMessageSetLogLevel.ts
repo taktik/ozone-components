503f1755b6af4a2b3b805228ef6721cff8e27820
Jason Van Malder <jvm@taktik.com>
2021-05-20 15:50:43 +0200

    Use enum instead of string

--- packages/ozone-helper/ozone-type/ozone/model/DeviceMessageSetLogLevel.ts
+++ packages/ozone-helper/ozone-type/ozone/model/DeviceMessageSetLogLevel.ts
@@ -2,9 +2,11 @@ import { DeviceMessage } from './DeviceMessage'
 
 import { OzoneType } from './Item'
 
+export enum Level { ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF }
+
 @OzoneType('device.message.setloglevel')
 export class DeviceMessageSetLogLevel extends DeviceMessage {
-	level: string
+	level: Level
 
 	constructor(src: DeviceMessageSetLogLevel) {
 		super(src)
