e0c6128c2214911db2ae5d09f900b0cf0df6d7cd
Loris Mancel <lm@taktik.com>
2021-10-25 15:37:25 +0200

    fix(model) Updated Recording model

--- packages/ozone-helper/ozone-type/ozone/model/Recording.ts
+++ packages/ozone-helper/ozone-type/ozone/model/Recording.ts
@@ -7,6 +7,7 @@ export class Recording extends Item {
 	start: Instant
 	stop: Instant
 	video?: UUID
+	state: 'READY' | 'EXCEEDING_QUOTA' | 'RECORDING' | 'PROCESSING' | 'GARBAGE_COLLECTED' | 'PENDING'
 
 	constructor(src: Recording) {
 		super(src)
@@ -15,5 +16,6 @@ export class Recording extends Item {
 		this.start = src.start
 		this.stop = src.stop
 		this.video = src.video
+		this.state = src.state
 	}
 }
