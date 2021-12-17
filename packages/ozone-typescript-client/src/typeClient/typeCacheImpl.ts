cc9399788124b1c78dac53a229431a21a7981479
Loris Mancel <lm@taktik.com>
2021-12-17 11:48:28 +0100

    fix(typeCache) do not fully import lodash
    only use the component we need: uniqBy

--- packages/ozone-typescript-client/src/typeClient/typeCacheImpl.ts
+++ packages/ozone-typescript-client/src/typeClient/typeCacheImpl.ts
@@ -1,4 +1,4 @@
-import { uniqBy } from 'lodash'
+import uniqBy from 'lodash/uniqBy'
 import { TypeDescriptor, FieldDescriptor, Item } from 'ozone-type'
 import { TypeCache } from './typeCache'
 import { Cache } from '../cache/cache'
